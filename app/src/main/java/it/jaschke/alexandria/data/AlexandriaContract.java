e162cebb93046327a3cd1e81c3c41d57ffdb7cd4
Sascha Jaschke <sascha@jaschke.it>
2014-12-22 16:58:32 +0100

    add contract

---
+++ app/src/main/java/it/jaschke/alexandria/data/AlexandriaContract.java
@@ -0,0 +1,45 @@
+package it.jaschke.alexandria.data;
+
+/**
+ * Created by saj on 22/12/14.
+ */
+
+import android.provider.BaseColumns;
+
+public class AlexandriaContract{
+
+    public static final class BookEntry implements BaseColumns {
+        public static final String TABLE_NAME = "books";
+
+        public static final String EAN = "ean";
+
+        public static final String TITLE = "title";
+
+        public static final String IMAGE_URL = "ean";
+
+
+
+    }
+
+    public static final class AuthorEntry implements BaseColumns {
+        public static final String TABLE_NAME = "authors";
+
+        public static final String EAN = "ean";
+
+        public static final String AUTHOR = "author";
+
+
+
+    }
+
+    public static final class CategoryEntry implements BaseColumns {
+        public static final String TABLE_NAME = "categories";
+
+        public static final String EAN = "ean";
+
+        public static final String CATEGORY = "category";
+
+
+    }
+
+}
