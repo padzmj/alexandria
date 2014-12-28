a683d0e27999c78fbed89e94ed33297de495cb05
Sascha Jaschke <sascha@jaschke.it>
2014-12-28 12:08:06 +0100

    fetch json with intent service

--- app/src/main/java/it/jaschke/alexandria/AddBook.java
+++ app/src/main/java/it/jaschke/alexandria/AddBook.java
@@ -1,5 +1,6 @@
 package it.jaschke.alexandria;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.text.Editable;
@@ -10,6 +11,8 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.EditText;
 
+import it.jaschke.alexandria.services.BookService;
+
 
 public class AddBook extends Fragment {
 
@@ -19,7 +22,7 @@ public class AddBook extends Fragment {
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(final LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View rootView = inflater.inflate(R.layout.fragment_scan, container, false);
 
         ean = (EditText) rootView.findViewById(R.id.ean);
@@ -40,6 +43,11 @@ public class AddBook extends Fragment {
                 if(s.length()<13){
                     return;
                 }
+
+                Intent bookIntent = new Intent(getActivity(), BookService.class);
+                bookIntent.putExtra(BookService.EAN,s.toString());
+                bookIntent.setAction(BookService.FETCH_BOOK);
+                getActivity().startService(bookIntent);
 
 
             }
---
+++ app/src/main/java/it/jaschke/alexandria/services/BookService.java
@@ -0,0 +1,127 @@
+package it.jaschke.alexandria.services;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.net.Uri;
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ */
+public class BookService extends IntentService {
+
+    private final String LOG_TAG = BookService.class.getSimpleName();
+
+    public static final String FETCH_BOOK = "it.jaschke.alexandria.services.action.FETCH_BOOK";
+    public static final String DELETE_BOOK = "it.jaschke.alexandria.services.action.DELETE_BOOK";
+
+    public static final String EAN = "it.jaschke.alexandria.services.extra.EAN";
+
+    public BookService() {
+        super("Alexandria");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+            if (FETCH_BOOK.equals(action)) {
+                final String ean = intent.getStringExtra(EAN);
+                fetchBook(ean);
+            } else if (DELETE_BOOK.equals(action)) {
+                final String ean = intent.getStringExtra(EAN);
+                deleteBook(ean);
+            }
+        }
+    }
+
+    /**
+     * Handle action Foo in the provided background thread with the provided
+     * parameters.
+     */
+    private void deleteBook(String ean) {
+        //throw new UnsupportedOperationException("Not yet implemented");
+    }
+
+    /**
+     * Handle action Baz in the provided background thread with the provided
+     * parameters.
+     */
+    private void fetchBook(String ean) {
+
+        if(ean.length()!=13){
+            return;
+        }
+
+        HttpURLConnection urlConnection = null;
+        BufferedReader reader = null;
+        String bookJson = null;
+
+        try {
+            final String FORECAST_BASE_URL = "https://www.googleapis.com/books/v1/volumes?";
+            final String QUERY_PARAM = "q";
+
+            final String ISBN_PARAM = "isbn:" + ean;
+
+            Uri builtUri = Uri.parse(FORECAST_BASE_URL).buildUpon()
+                    .appendQueryParameter(QUERY_PARAM, ISBN_PARAM)
+                    .build();
+
+            URL url = new URL(builtUri.toString());
+
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
+
+            InputStream inputStream = urlConnection.getInputStream();
+            StringBuffer buffer = new StringBuffer();
+            if (inputStream == null) {
+                // Nothing to do.
+                return;
+            }
+
+            reader = new BufferedReader(new InputStreamReader(inputStream));
+            String line;
+            while ((line = reader.readLine()) != null) {
+                // Since it's JSON, adding a newline isn't necessary (it won't affect parsing)
+                // But it does make debugging a *lot* easier if you print out the completed
+                // buffer for debugging.
+                buffer.append(line);
+                buffer.append("\n");
+            }
+
+            if (buffer.length() == 0) {
+                // Stream was empty.  No point in parsing.
+                return;
+            }
+            bookJson = buffer.toString();
+            Log.d("my-tag",bookJson);
+        } catch (Exception e) {
+            Log.e(LOG_TAG, "Error ", e);
+        } finally {
+            if (urlConnection != null) {
+                urlConnection.disconnect();
+            }
+            if (reader != null) {
+                try {
+                    reader.close();
+                } catch (final IOException e) {
+                    Log.e(LOG_TAG, "Error closing stream", e);
+                }
+            }
+
+        }
+
+    }
+}
